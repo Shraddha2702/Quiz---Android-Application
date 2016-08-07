248da412aadbb24f3d78fee5d9a64df806002f4b
Shraddha K <Shraddha K>
2016-08-07 20:54:56 +0530

    Committing

--- main/java/com/example/shraddha/quiz/CalculatorActivity.java
+++ main/java/com/example/shraddha/quiz/CalculatorActivity.java
@@ -4,8 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
 public class CalculatorActivity extends AppCompatActivity {
     TextView onesub1,onesub2,twosub1,threesub1,foursub1,fivesub1,sixsub1,sevensub1,eightsub1,ninesub1,tensub1;
+    int height = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,53 +21,60 @@ public class CalculatorActivity extends AppCompatActivity {
 
         Bundle b=getIntent().getExtras();
 
-        String he=b.getString("height");
-        int height = Integer.parseInt(he);
-        String we = b.getString("weight");
-        int weight = Integer.parseInt(we);
-        Long t = b.getLong("time");
+        String post=b.getString("post");
+        int height = b.getInt("height");
+        int weight = b.getInt("weight");
+        int day = b.getInt("day");
+        int month = b.getInt("month");
+        int year = b.getInt("year");
+        long t = b.getLong("time");
 
 
 
 
+
+
+
+
+        //System.out.println("The values are:" + year + month + day + t);
 
         onesub1 = (TextView) findViewById(R.id.one_sub1);
-        onesub1.setText("Your heart has beaten " + t*72 + " times");
+        onesub1.setText("Your heart has beaten " + t*72 + post + " times");
 
 
         onesub2 = (TextView) findViewById(R.id.one_sub2);
-        onesub2.setText("A Whale's heart has beaten " + t*8 + " times");
+        onesub2.setText("A Whale's heart has beaten " + t*8 + post + " times");
 
 
         twosub1 = (TextView) findViewById(R.id.two_sub1);
-        twosub1.setText("Flash has completed " + 3600*t + " around the Earth");
+        twosub1.setText("Flash has completed " + 3*t + "Quintillion trips around the Earth");
 
 
         threesub1 = (TextView) findViewById(R.id.three_sub1);
-        threesub1.setText("A Humming Bird's heart has beaten " + 250*t + " times");
+        threesub1.setText("A Humming Bird's heart has beaten " + 250*t + post+ " times");
 
 
         foursub1 = (TextView) findViewById(R.id.four_sub1);
-        foursub1.setText("You are " + (88800+t)/(365*86400) + " years old in Mars");
+        foursub1.setText("You are " + Math.round(year/1.88) + " years old in Mars");
 
 
         fivesub1 = (TextView) findViewById(R.id.five_sub1);
-        fivesub1.setText( "Ant has family of " + 579.1* t/(86400*365)+ " generations");
+        fivesub1.setText( "Ant has family of " + 667*(year)+ " generations");
 
         sixsub1 = (TextView) findViewById(R.id.six_sub1);
-        sixsub1.setText("A redwood's growth in your lifetime " + 2.6917*height);
+        sixsub1.setText("A redwood's growth in your lifetime " + height/100 * 3.5144 + "m");
 
 
         sevensub1 = (TextView) findViewById(R.id.seven_sub1);
-        sevensub1.setText("Population growth in your lifetime " + t/(86400*365)* 7.4* 1.3 + " Billion");
+        sevensub1.setText("Population has increased by " + Math.round((year)*  1.013) + "% in your lifetime");
 
 
         eightsub1 = (TextView) findViewById(R.id.eight_sub1);
-        eightsub1.setText("You have walked " + 3000*(t/86400) + " steps in your lifetime");
+        eightsub1.setText("You have walked " + Math.abs(3000*(year*365)) + " steps in your lifetime");
 
 
         ninesub1 = (TextView) findViewById(R.id.nine_sub1);
-        ninesub1.setText("Sea level rising in your lifetime " + t/(86400*365)*0.0032 + "%");
+        ninesub1.setText("Sea level rising in your lifetime " + (year)*0.0032 + "%");
 
 
         tensub1 = (TextView) findViewById(R.id.ten_sub1);
--- main/java/com/example/shraddha/quiz/HowmuchActivity.java
+++ main/java/com/example/shraddha/quiz/HowmuchActivity.java
@@ -4,20 +4,23 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
-public class HowmuchActivity extends Activity {
-    long time;
-    DatePicker datePicker;
+public class HowmuchActivity extends AppCompatActivity {
+    EditText y , m, d;
     EditText Height;
     EditText Weight;
     Button b1;
+    int flag = 0;
+    int birthday=0, birthmonth=0, birthyear=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,51 +28,144 @@ public class HowmuchActivity extends Activity {
         setContentView(R.layout.activity_howmuch);
 
 
-        datePicker = (DatePicker) findViewById(R.id.datePicker);
-
-        int birthday = datePicker.getDayOfMonth();
-        int birthmonth = datePicker.getMonth() + 1;
-        int birthyear = datePicker.getYear();
 
 
-        final Calendar c = Calendar.getInstance();
-        int currentyear = c.get(Calendar.YEAR);
-        int currentmonth = c.get(Calendar.MONTH);
-        int currentday = c.get(Calendar.DAY_OF_MONTH);
 
-        int year = currentyear - birthyear;
-        int month = currentmonth - birthmonth;
-        int day = currentday - birthday;
-        time = ((year * 365) + (month * 30) + day) * (24 * 36);
+        Height = (EditText) findViewById(R.id.height);
+        Weight = (EditText) findViewById(R.id.weight);
 
-        Height = (EditText) findViewById(R.id.editheight);
+        y = (EditText) findViewById(R.id.year);
+        m = (EditText) findViewById(R.id.month);
+        d = (EditText) findViewById(R.id.date);
 
-        Weight = (EditText) findViewById(R.id.editweight);
 
-        b1 = (Button) findViewById(R.id.btnmain);
-    b1.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Intent intent=new Intent(HowmuchActivity.this,CalculatorActivity.class);
-            String h = Height.getText().toString();
-            String w=Weight.getText().toString();
-            Bundle b=new Bundle();
 
-            b.putLong("time",time);
-            b.putString("height",h);
-            b.putString("weight",w);
+        b1 = (Button) findViewById(R.id.calculate);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String postfix = "";
+                flag = 0;
+                Intent intent = new Intent(HowmuchActivity.this, CalculatorActivity.class);
 
-            intent.putExtras(b);
+                String h = Height.getText().toString();
+                int height = Integer.parseInt(h);
+                // checking height
+                if(height <= 0 || height > 270){
+                    flag = 1;
+                }
 
-            startActivity(intent);
-            finish();
-        }
-    });}
+
+                String w = Weight.getText().toString();
+                int weight = Integer.parseInt(w);
+                // checking weight
+                if(weight <= 0 || weight > 442 ){
+                    flag = 1;
+                }
+
+                String years = y.getText().toString();
+                String months = m.getText().toString();
+                String days = d.getText().toString();
+
+                birthday = Integer.parseInt(days);
+                birthmonth = Integer.parseInt(months);
+                birthyear = Integer.parseInt(years);
+
+                Calendar c = Calendar.getInstance();
+                int currentyear = c.get(Calendar.YEAR);
+                int currentmonth = c.get(Calendar.MONTH);
+                int currentday = c.get(Calendar.DAY_OF_MONTH);
+
+
+                int year = currentyear - birthyear;
+                // year check
+                if(year == 0){
+                    year = 1;
+                }
+                if(birthyear < 1900 || birthyear > currentyear){
+                    flag = 1;
+                }
+
+                int month = Math.abs(currentmonth - birthmonth);
+                // month check
+                if(birthmonth <= 0 || birthmonth > 12){
+                    flag = 1;
+                }
+
+                int day = Math.abs(currentday - birthday);
+                // date check
+                if(birthday <=0 || birthday >= 31){
+                    flag = 1;
+                }
+
+
+                // checking dates
+                if(birthmonth == 1 || birthmonth == 3 || birthmonth == 5 || birthmonth == 7 || birthmonth == 8 || birthmonth == 10 || birthmonth == 12){
+
+                    if(birthday > 31){
+                        flag = 1;
+                    }
+                    else {
+                        day = day + 1;
+                    }
+                }
+                else if(birthmonth == 4 || birthmonth == 6 || birthmonth == 9 || birthmonth == 11){
+                    if(birthday > 30){
+                        flag = 1;
+                    }
+                }
+                else if(birthmonth == 2){
+                    if(birthday > 28 ){
+                        flag = 1;
+                    }
+                    if(birthyear%4 == 0 && birthday > 29){
+                        flag = 1;
+                    }
+                    else{
+                        day++;
+                    }
+                }
+
+
+                //converting all time units in second
+                long t = ((year * 365) + (month * 30) + day) * (24 * 60);
+
+                // Millions and Billions
+                if (t >= 1000000000) {
+                    t = t / 1000000000;
+                    postfix = "Billion";
+                } else if (t >= 1000000) {
+                    t = t / 1000000;
+                    postfix = "Million";
+                }
+
+
+
+                // Final check all together
+                if (flag == 0) {
+                    Bundle b = new Bundle();
+
+
+                    b.putInt("height", height);
+                    b.putInt("weight", weight);
+                    b.putInt("year", year);
+                    b.putInt("month", month);
+                    b.putInt("day", day);
+                    b.putLong("time", t);
+                    b.putString("post", postfix);
+
+
+                    intent.putExtras(b);
+
+                    startActivity(intent);
+                    finish();
+                }
+                else{
+                    Toast.makeText(HowmuchActivity.this, "Invalid input format entered", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });}
 }
-
-
-
-
 
 
 
